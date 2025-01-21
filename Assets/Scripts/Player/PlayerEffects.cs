d751c169da9b298b9fdf01d4ed93f4f770082293
Taikwy <taikwy.twa@gmail.com>
2025-01-20 17:00:10 -0800

    camera plus some sprites

---
+++ Assets/Scripts/Player/PlayerEffects.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEffects : MonoBehaviour
+{
+    
+    public void Shake(){}
+    public void Wobble(){}
+}
