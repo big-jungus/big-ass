d6d4deb2263b80abbf6af59df87122f722c17578
big-jungus <matthewjung1010@gmail.com>
2025-01-21 17:41:06 -0800

    thugshaker

---
+++ Assets/Scripts/Player/PlayerVisuals.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisuals : MonoBehaviour
+{
+    [SerializeField] private Vector2 maxMovementConstraints;
+    [SerializeField] private AnimationCurve movementSpeedCurve;
+
+    private Vector2 currentMovementConstraints;
+    private float currentMovementSpeed;
+
+    private bool shake;
+
+    [Header("Return Animation")]
+    [SerializeField] private float returnDuration;
+    [SerializeField] private AnimationCurve returnCurve;
+
+    private void Start()
+    {
+        PlayerManager.playerManager.playerController.Charging += Shake;
+        PlayerManager.playerManager.playerController.ChargeStarted += Started;
+        PlayerManager.playerManager.playerController.ChargeEnded += Ended;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerManager.playerManager.playerController.Charging -= Shake;
+        PlayerManager.playerManager.playerController.ChargeStarted -= Started;
+        PlayerManager.playerManager.playerController.ChargeEnded -= Ended;
+    }
+
+    private void Started()
+    {
+        shake = true;
+        StartCoroutine(ShakeAnimation());
+    }
+
+    private void Shake(float currentChargeAmount)
+    {
+        float percent = currentChargeAmount / PlayerManager.playerManager.playerStats.maxChargeDuration;
+        currentMovementSpeed = movementSpeedCurve.Evaluate(percent);
+        currentMovementConstraints = Vector2.Lerp(Vector2.zero, maxMovementConstraints, percent);
+    }
+
+    private void Ended()
+    {
+        shake = false;
+
+        StartCoroutine(ReturnAnimation());
+    }
+
+    private IEnumerator ShakeAnimation()
+    {
+        while (shake)
+        {
+            yield return MovePosition();
+        }
+    }
+
+    private IEnumerator MovePosition()
+    {
+        Vector2 randLocation = new Vector2(Random.Range(-currentMovementConstraints.x, currentMovementConstraints.x), Random.Range(-currentMovementConstraints.y, currentMovementConstraints.y));
+        Vector2 currLocation = transform.localPosition;
+
+        float currentTime = 0f;
+        while (currentTime < 1f)
+        {
+            currentTime += Time.deltaTime * currentMovementSpeed;
+            yield return null;
+
+            transform.localPosition = Vector2.Lerp(currLocation, randLocation, currentTime);
+        }
+    }
+
+    private IEnumerator ReturnAnimation()
+    {
+        Vector3 startingPosition = transform.localPosition;
+
+        float currentTime = 0f;
+        while (currentTime < returnDuration)
+        {
+            yield return null;
+            currentTime += Time.deltaTime;
+
+            transform.localPosition = Vector3.Lerp(startingPosition, Vector3.zero, returnCurve.Evaluate(currentTime / returnDuration));
+        }
+    }
+}
