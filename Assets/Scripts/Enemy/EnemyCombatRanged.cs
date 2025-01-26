a0c58aab4220ac1af329b31e437f75bdcec69d61
big-jungus <matthewjung1010@gmail.com>
2025-01-26 01:09:56 -0800

    enemy ranged different shots

--- Assets/Scripts/Enemy/EnemyCombatRanged.cs
+++ Assets/Scripts/Enemy/EnemyCombatRanged.cs
@@ -10,16 +10,84 @@ public class EnemyCombatRanged : CombatBase
     private bool canAttack = true;
     private Transform target;
 
+    [SerializeField] private ShootingTypes shootingType;
+    [SerializeField] private TargetingTypes targetingType;
+
+    [SerializeField] private Vector2 shootDir;
+    [SerializeField] private int numBullets;
+    [SerializeField] private float maxAngle;
+
     private void FixedUpdate()
     {
         if (canAttack && target != null)
         {
-            GameObject bullet = Instantiate(bulletPrefab, transform, false);
-            bullet.transform.localPosition = Vector3.zero;
-            Vector2 launchVector = (target.position - transform.position).normalized * launchSpeed;
-            bullet.GetComponent<Rigidbody2D>().velocity = launchVector;
+            switch (shootingType)
+            {
+                case ShootingTypes.Single:
+                    SingleShot();
+                    break;
+
+                case ShootingTypes.Fan:
+                    FanShot();
+                    break;
+
+                case ShootingTypes.Spread:
+                    SpreadShot();
+                    break;
+            }
 
             StartCoroutine(AttackCooldown());
+        }
+    }
+
+    private void SingleShot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform, false);
+        bullet.transform.localPosition = Vector3.zero;
+        bullet.GetComponent<Rigidbody2D>().velocity = GetLaunchVector();
+    }
+
+    private void FanShot()
+    {
+        Vector2 centerVector = GetLaunchVector();
+        float deg = maxAngle / (numBullets - 1);
+
+        for (int i = 0; i < numBullets; i++)
+        {
+            float targetAngle = (maxAngle / 2) - (deg * i);
+            Vector2 newVector = Quaternion.Euler(0, 0, targetAngle) * centerVector;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform, false);
+            bullet.transform.localPosition = Vector3.zero;
+            bullet.GetComponent<Rigidbody2D>().velocity = newVector;
+        }
+    }
+
+    private void SpreadShot()
+    {
+        Vector2 centerVector = GetLaunchVector();
+
+        for (int i = 0; i < numBullets; i++)
+        {
+            float targetAngle = Random.Range(-1 * maxAngle / 2, maxAngle / 2);
+            Vector2 newVector = Quaternion.Euler(0, 0, targetAngle) * centerVector;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform, false);
+            bullet.transform.localPosition = Vector3.zero;
+            bullet.GetComponent<Rigidbody2D>().velocity = newVector;
+        }
+    }
+
+    private Vector2 GetLaunchVector()
+    {
+        switch (targetingType)
+        {
+            case TargetingTypes.Player:
+                return (target.position - transform.position).normalized * launchSpeed;
+            case TargetingTypes.Direction:
+                return shootDir.normalized * launchSpeed;
+            default:
+                return (target.position - transform.position).normalized * launchSpeed;
         }
     }
 
@@ -45,5 +113,18 @@ public class EnemyCombatRanged : CombatBase
         canAttack = false;
         yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
+    }
+
+    private enum ShootingTypes
+    {
+        Single,
+        Fan,
+        Spread
+    }
+
+    private enum TargetingTypes
+    {
+        Player,
+        Direction
     }
 }
